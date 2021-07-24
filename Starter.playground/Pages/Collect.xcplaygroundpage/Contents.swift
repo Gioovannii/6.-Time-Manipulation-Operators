b82deb38f3a5f5882199e46a70e2a13aedc83048
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-24 11:34:19 +0200

    flat map

--- Starter.playground/Pages/Collect.xcplaygroundpage/Contents.swift
+++ Starter.playground/Pages/Collect.xcplaygroundpage/Contents.swift
@@ -42,7 +42,9 @@ let collectMaxCount = 2
 let colectedPublisher2 = sourcePublisher
 
     .collect(.byTimeOrCount(DispatchQueue.main, .seconds(collectTimeStride), collectMaxCount))
-   
+    .flatMap { dates in
+        dates.publisher
+    }
 
 //: [Next](@next)
 /*:
