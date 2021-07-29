0a9bf3a921fb3eb84995bd2ae2a139855d66c686
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-07-29 18:04:39 +0200

    Set two subscriptions

--- Starter.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
+++ Starter.playground/Pages/MeasureInterval.xcplaygroundpage/Contents.swift
@@ -21,6 +21,12 @@ subject.displayEvents(in: subjectTimeline)
 measureSubject.displayEvents(in: measureTimeline)
 
 
+let subscription1 = subject.sink { print("+\(deltaTime)s: Subject emitted \($0)")
+}
+
+let subscription2 = measureSubject.sink {
+    print("+\(deltaTime)s: Measure emitted: \($0)")
+}
 
 /*:
  Copyright (c) 2020 Razeware LLC
